c591c3d525ab3e7b869107bf4936c64c2496b123
Toni Hackett <hckt76@gmail.com>
2019-08-08 12:54:45 +0200

    hola

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,6 +11,18 @@ router.get('/about', (req, res)=>{
     res.render('about')
 })
 
+router.get('/contact', (req, res)=>{
+    res.render('contact')
+})
+
+router.get('/entities', (req, res)=>{
+    res.render('entities')
+})
+
+router.get('/blog', (req, res)=>{
+    res.render('blog')
+})
+
 /// cuando buscas cualquier ruta - parecido a un error 404 
 // router.get('*', (req, res) => {
 //     res.send('Dont route this route')
